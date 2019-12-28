59ec9a5ee7ab5f2f887a3ab0e2aa83d7a0a5a532
Chiranjibi Lawati <chiranjibilawati08@gmail.com>
2019-12-28 21:55:57 +0545

    the site contains header and footer

---
+++ footballclubsite/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './Hoc/Layout';
+
+const Routes = (props) => {
+  return (
+      <Layout>
+        decond line
+      </Layout>
+    )
+}
+
+export default Routes;
